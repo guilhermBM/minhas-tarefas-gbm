b2174995e4bd993396bc64e0cb6b47351475531c
guilherme <guilherme@teste.com>
2025-04-13 14:34:02 -0300

    Finalização do projeto 1 com React.

---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,93 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+
+import * as enums from '../../utils/enums/tarefa'
+
+type TarafasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarafasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar JavaScript revendo o ecercício do módulo 7',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JavaScript'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar material de apoio',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TypeScript'
+    },
+    {
+      id: 3,
+      descricao: 'Práticar a construção de um landing page',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar BootStrap'
+    }
+  ]
+}
+
+const tarefasSlice = createSlice({
+  name: 'tarefas',
+  initialState,
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state.itens = [
+        ...state.itens.filter((tarefa) => tarefa.id !== action.payload)
+      ]
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
+      // tarefaParaEditar = action.payload
+    },
+    cadastrar: (state, action: PayloadAction<Omit<Tarefa, 'id'>>) => {
+      const tarefaJaExiste = state.itens.find(
+        (tarefa) =>
+          tarefa.titulo.toLocaleLowerCase() ===
+          action.payload.titulo.toLocaleLowerCase()
+      )
+
+      if (tarefaJaExiste) {
+        alert('Já existe uma tarefa com esse nome')
+      } else {
+        const ultimaTarefa = state.itens[state.itens.length - 1]
+
+        const tarefaNova = {
+          ...action.payload,
+          id: ultimaTarefa ? ultimaTarefa.id + 1 : 1
+        }
+        state.itens.push(tarefaNova)
+      }
+    },
+    alteraStatus: (
+      state,
+      action: PayloadAction<{ id: number; finalizado: boolean }>
+    ) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa].status = action.payload.finalizado
+          ? enums.Status.CONCLUIDA
+          : enums.Status.PENDENTE
+      }
+    }
+  }
+})
+
+export const { remover, editar, cadastrar, alteraStatus } = tarefasSlice.actions
+
+export default tarefasSlice.reducer
---
+++ src/styles/index.ts
@@ -0,0 +1,58 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import variaveis from './variaveis'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0%;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+`
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 224px auto;
+`
+
+export const MainContainer = styled.main`
+  padding: 0 40px;
+  height: 100vh;
+  overflow-y: scroll;
+`
+export const Titulo = styled.h2`
+  display: block;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+export const Campo = styled.input`
+  padding: 8px;
+  border-radius: 8px;
+  width: 100%;
+  background-color: #fff;
+  font-weight: bold;
+  color: #666666;
+  border-color: #666666;
+`
+
+export const Botao = styled.button`
+  font-weight: bold;
+  font-size: 12px;
+  padding: 8px 12px;
+  border-radius: 8px;
+  margin-right: 8px;
+  color: #fff;
+  background-color: ${variaveis.azulEscuro};
+  border: none;
+  cursor: pointer;
+`
+
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.verde};
+`
+
+export default EstiloGlobal
